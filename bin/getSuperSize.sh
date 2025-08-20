b9521d278272cf2b9e9b61d13b90b7e93d3df8c2
Ashish Sangar <ashishsangar5@gmail.com>
2025-08-20 17:55:16 +0000

    added support for cn rui

--- bin/getSuperSize.sh
+++ bin/getSuperSize.sh
@@ -3,14 +3,15 @@ device_code=$1
 case $device_code in
 	OnePlus9R) size=9932111872;;
 	OnePlus8T) size=7516192768;;
-	RE54E4L1 | RMX3371) size=11274289152;;
-    OnePlus8 | OnePlus8Pro) size=15032385536;;
+	#realme GT NEO 3T/realme Q5 Pro
+	RE54E4L1 | RMX3371 | RE5744 | RMX3372) size=11274289152;;
+        OnePlus8 | OnePlus8Pro) size=15032385536;;
 	#Oppo find X3
 	OP4E5D | OnePlus9 | OnePlus9Pro) size=11190403072;;
-    #Oppo Find X3 Pro
-    OP4E3F) size=11186208768;;
+        #Oppo Find X3 Pro
+        OP4E3F) size=11186208768;;
 	# Oplus ACE3V
-	 OP5CFBL1) size=16106127360;; 
+	OP5CFBL1) size=16106127360;; 
 	#Others
 	*) size=11811160064;;
 esac
